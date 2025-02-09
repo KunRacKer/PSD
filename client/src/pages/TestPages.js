eae66d6c55873ebb19cd431f78c00613058dc80e
unknown <helyxrulk@gmail.com>
2025-02-10 01:10:28 +0700

    fetch api ada di testpages

--- client/src/pages/TestPages.js
+++ client/src/pages/TestPages.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const TestPages = () => {
+  const getAllData = async () => {
+    try {
+      const res = await fetch(
+        "https://psd-backend-nodejs.vercel.app/api/get/all/data"
+      );
+
+      const data = res.json();
+
+      console.log("Semua data : ", data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+
   return (
     <div>
-        <h1>asdasdsad</h1>
+      <h1>asdasdsad</h1>
     </div>
-  )
-}
+  );
+};
 
-export default TestPages
+export default TestPages;
